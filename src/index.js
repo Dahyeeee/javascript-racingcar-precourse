e9a18be66f5334766d08e020aa3f9d397356e165
Dahye <06robin11@gmail.com>
2022-04-13 21:22:54 +0900

    race & winner function added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ function checkValidity(event){
     event.preventDefault();
     const carnamesstring = document.querySelector('#car-names-input').value;
     const carnamesarr = carnamesstring.split(',');
-    if(!carnamesstring.includes(',')){
+    const regex = /.+\,.+\,.+\,.+/
+    if(carnamesstring !== regex){
         alert( "You need to type , between cars' names.")
     }
     else if(!carnamesarr.every((item)=>item.length <= 5)){
@@ -25,31 +26,76 @@ function getPlayTime(){
     return Number(playtime);
 }
 
-
-function race(carsname){
-    for(let i=0; i<carsname.length ; i++){
-        carsname[i] = [];
+class Cars{
+    constructor(name){
+        this.name = name
+        this.scores = []
     }
-    for(let i=0 ; i<carsname.length ; i++){
+
+    move(){
         let randomNumber = MissionUtils.Random.pickNumberInRange(1,9);
         if(randomNumber > 4){
-            carsname[i].push('-')
-            console.log(carsname[i]);
+            this.scores.push('-')
+           }
+    }
+
+    accumlatedScore(round){
+        return this.scores.slice(0,round+1).join('');
+  //      return scroeWithRound.reduce((acc,val)=>acc+val,0);
+    }
+}
+
+function race(cars,playtime){
+    const result = []
+    for(let i=0 ; i<playtime ; i++){
+       for(let car of cars){
+          car.move();
+    }
+} return cars;
+}
+
+function showResult(result,playtime){
+  let resultPhrase= ''
+  for(let i=0; i<playtime ; i++){
+    for(let car of result){
+        resultPhrase += `${car.name} : ${car.accumlatedScore(i)}<br>`
+    }
+    resultPhrase += `<br>`
+    }return resultPhrase;
+}
+
+function findWinner(result){
+    let finalScores=[];
+    for(let car of result){
+        finalScores.push(car.scores);
+    }
+    let max = finalScores[0].length;
+    let winners = ''
+    finalScores.forEach((item)=> max = Math.max(max, item.length));
+    for(let i=0; i<result.length ; i++){
+        if(finalScores[i].length === max){
+        winners += `${result[i].name} `;
         }
-        else{
-            console.log(carsname[i]);
-        }
-    }}
+    }
+    return winners;
+}
+
+const onScreen = document.querySelector('#result');
+const onScreenWinner = document.querySelector('#racing-winners')
 
 function carRaceGame(event){
     event.preventDefault();
-    const carsname = getCarNames();
+    const carnames = getCarNames();
+    const cars = []
+    for(const carname of carnames){     
+        cars.push(new Cars(carname));
+    }
     const playtime = getPlayTime();
-    const racecars= race(carsname, playtime);
-//     const showresult = showResult(racecars);
-//     return showresult;
-// }
- return racecars;
+    const result= race(cars,playtime);
+    const showresult = showResult(result,playtime);
+    const winner = findWinner(result);
+    onScreen.innerHTML = showresult;
+    onScreenWinner.textContent = winner;
 }
 
 const playtimebtn = document.querySelector('#racing-count-submit');
